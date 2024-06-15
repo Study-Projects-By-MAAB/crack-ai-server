d96369beb8a302252eec758478733316b2ce14b9
ferdouszihad <f.zihad333@gmail.com>
2024-06-15 22:17:23 +0600

    painting generation implemented

--- index.js
+++ index.js
@@ -2,43 +2,24 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { connect } = require("./utils/dbconnect");
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+//global middleware
+app.use(cors());
+app.use(express.json());
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+connect();
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const form = `
-
-  <form method="POST" action="/prompt"> 
-   <textarea name="prompt" id="prompt"></textarea>
-   <button type="submit">Generate JSON </button>
-  </form>
-
-
-`;
-
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/prompt", async (req, res) => {
-  res.send(form);
-});
-
-app.post("/prompt", async (req, res) => {
-  let { prompt } = req.body;
-  prompt = prompt + ". data will be in json stringify version. no extra text";
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  const cleanJsonString = text.replace(/\\n/g, "").replace(/\\/g, "");
-  res.send({ data: cleanJsonString, status: 200 });
-});
+//routes
+const paintingRoutes = require("./routes/paintings.route");
+app.use("/paintings", paintingRoutes);
 
 app.get("/", (req, res) => {
-  res.send({ data: "server Running", status: 200 });
+  res.send({
+    data: "server Running",
+    status: 200,
+  });
 });
 
 app.listen(port, () => {
